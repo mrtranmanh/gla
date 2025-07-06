0bce93d4e10b7cfd6ceeb45158612ade73a8e028
playerinvn <mr.taduma@gmail.com>
2025-07-06 09:01:09 +0700

    Update aut click

---
+++ tdmtoolsgla/js/autoclick.js
@@ -0,0 +1,29 @@
+// buy market
+function buy_market() {
+    const elements = document.querySelectorAll('#market_table > .awesome-button');
+    let delay = 0;
+
+    elements.forEach((element, index) => {
+        setTimeout(() => {
+            element.click();
+            console.log(`Clicked element ${index + 1}`);
+        }, delay);
+        delay += 200;
+    });
+}
+buy_market();
+
+//sell guild market
+function buy__guild_market() {
+    const elements = document.querySelectorAll('#inv > div');
+    let delay = 0;
+
+    elements.forEach((element, index) => {
+        setTimeout(() => {
+            element.click();
+            console.log(`Clicked element ${index + 1}`);
+        }, delay);
+        delay += 200;
+    });
+}
+buy__guild_market();
