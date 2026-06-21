8545d39557d483458533b5b661b6d577ace3e460
Tran Manh <mr.taduma@gmail.com>
2026-06-21 15:50:29 +0700

    feat: restrict page reload to only occur when autoGoActive session flag is enabled

--- tdmtoolsgla/js/reloadpage.js
+++ tdmtoolsgla/js/reloadpage.js
@@ -4,7 +4,13 @@ const shouldEnableReload = enableReloadpage === 1 ? true : false;
 if (shouldEnableReload) {
     console.log('reloadpage co hoat dong');
     setInterval(function () {
-        location.reload();
+        const autoGoActive = sessionStorage.getItem('autoGoActive') === "true";
+        if (autoGoActive) {
+            console.log('autoGoActive is active -> reloadpage');
+            location.reload();
+        } else {
+            console.log('autoGoActive is inactive -> skip reloadpage');
+        }
     }, 3 * 60 * 1000);
 } else {
     console.log('reloadpage khong hoat dong');
